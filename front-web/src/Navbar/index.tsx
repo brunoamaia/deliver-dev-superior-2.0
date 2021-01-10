0cf803034c00c5bcb78f31fc040e764a48707417
brunoamaia <brunoamaia94@gmail.com>
2021-01-10 17:11:19 -0300

    Link to github

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -3,12 +3,16 @@ import './styles.css'
 import React from 'react';
 import {ReactComponent as Logo} from './logo.svg'
 import { Link } from 'react-router-dom';
+import {ReactComponent as GitHub} from './github.svg'
 
 function Navbar() {
   return(
     <nav className="main-navbar">
       <Logo />
       <Link to="/" className="logo-text">DS Delivery</Link>
+      <a href="https://github.com/brunoamaia/deliver-dev-superior-2.0" target="_new" className="git-link" >
+        <GitHub className="github-link" />
+      </a>
     </nav>
   )
 }
